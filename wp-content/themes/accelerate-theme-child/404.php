e12676ddf538e53e76968c86e12f52190f169676
joel <joel.santos21@gmail.com>
2016-08-04 23:54:55 -0500

    updated 404 page

--- wp-content/themes/accelerate-theme-child/404.php
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -5,10 +5,15 @@ get_header(); ?>
 
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php the_content(); ?>
-			<?php endwhile; // end of the loop. ?>
+			
+				<h1>Whoops, Took a Wrong Turn...</h1>
 
+				<img src="/wp-content/themes/accelerate-theme-child/img/404-pic.jpg" alt="404-pic" width="775" height="284" class="aligncenter size-full wp-image-47380" />
+
+				<p>Sorry, this page no longer exists, never existed or has been moved. We feel like complete jerks for totally misleading you.</p>
+
+				<p>Feel free to take a look around our <a href="/blog/">blog</a> or some of our featured <a href="/case-studies/">work</a>.</p>
+			
 		</div><!-- #content -->
 	</div><!-- #primary -->
 
